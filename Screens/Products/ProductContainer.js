3dba14aeb113f2d660a6bfd24ccd816b8aae723b
Danyspb4ever <danyspb4ever@gmail.com>
2022-12-01 21:38:58 +0000

    name brand ok

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -6,11 +6,32 @@ const data = require('../../assets/data/products.json');
 const ProductContainer = ()=>{
     
     const [products, setProducts] = useState([]);
+    useEffect(()=> {
+        setProducts(data);
+
+        return ()=>{
+            setProducts([])
+        }
+    }, [])
     return(
         <View>
             <Text>Product container</Text>
+            <View style={styles.container}>
+                <FlatList 
+                horizontal
+                data={products}
+                renderItem={({item}) =>(<Text>{item.brand}</Text>)}
+                keyExtractor={item=> item.name}
+                />
+            </View>
         </View>
     )
 }
-
+const styles = StyleSheet.create({
+    container: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: 100,
+    }
+})
 export default ProductContainer
