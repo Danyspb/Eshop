b71fb9095204cc06fb7e270e30754dab2b3bdce5
Danyspb4ever <danyspb4ever@gmail.com>
2023-01-30 16:22:26 +0000

    project ok

---
+++ Screens/Products/ProductList.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { TouchableOpacity, View, Dimensions, StyleSheet, Text} from "react-native";
+import ProductCard from "./ProductCard";
+
+var {width} = Dimensions.get('window');
+
+const ProductList = (props)=>{
+    const {item} = props;
+   
+    return(
+        <TouchableOpacity style={styles.container}>
+            <View style={{width: width /2, backgroundColor: 'gainsboro' }}>
+                <ProductCard
+                 {...item} />
+            </View>
+        </TouchableOpacity>
+    )
+}
+const styles = StyleSheet.create({
+    container:{
+        width: '50%',
+
+    }
+})
+
+export default ProductList;
