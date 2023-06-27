248503e44c0c51fe1611197481114f41e080a35e
Danyspb4ever <danyspb4ever@gmail.com>
2023-06-27 00:30:40 +0000

    feat: add search bar on the component

--- Screens/Products/ProductContainer.js
+++ Screens/Products/ProductContainer.js
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { FlatList, StyleSheet, View } from "react-native";
 import ProductList from "./ProductList";
-
+import { VStack, Input, Icon, NativeBaseProvider } from "native-base";
+import { MaterialIcons } from "@expo/vector-icons";
 
 const data = require('../../assets/data/products.json');
 
@@ -16,20 +17,22 @@ const ProductContainer = () =>{
     }, [])
 
     return (
-        
+        <NativeBaseProvider>
+            <VStack>
+                <Input placeholder="Search" width="100%" borderRadius="4" py="3" px="1" fontSize="14" InputLeftElement={<Icon m="2" ml="3" size="6" color="gray.400" as={<MaterialIcons name="search" />} />} />
+            </VStack>
             <View style={styles.container}>
                 <FlatList 
                     numColumns={2}
                     data={products}
                     renderItem={({item})=>
-                        <ProductList key={item.id} item={item}
-                        />}
-                    keyExtractor={item => item.name}
-                    /> 
-            </View>
-        
-    )
+                    <ProductList key={item.id} item={item}
+                />}
+                    keyExtractor={item => item.name}/> 
+                </View>
     
+        </NativeBaseProvider>
+    )
 }
 const styles = StyleSheet.create({
     container: {
